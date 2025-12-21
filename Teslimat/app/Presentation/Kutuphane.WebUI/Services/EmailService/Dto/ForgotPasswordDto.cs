3d072be11dccdc746edbccdbd1fba3c152406f22
Erkan Özgün <eozgun65@gmail.com>
2025-12-21 16:34:52 +0300

    Final

--- Teslimat/app/Presentation/Kutuphane.WebUI/Services/EmailService/Dto/ForgotPasswordDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Kutuphane.WebUI.Services.EmailService.Dto
-{
-    public class ForgotPasswordDto
-    {
-        public string Email { get; set; }
-    }
-}
--- Teslimat/app/Presentation/Kutuphane.WebUI/Services/EmailService/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Kutuphane.WebUI.Services.EmailService
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string toEmail, string subject, string body);
-    }
-}
